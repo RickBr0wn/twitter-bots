0b1cf4291f04bc8931aa70b1823b417d1c7a1507
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-26 13:12:15 +0100

    Working @ 1 tweet per minute

--- shiffmans-twitter-bot/bot.js
+++ shiffmans-twitter-bot/bot.js
@@ -3,4 +3,28 @@ const twitConfigKeys = require('./config/keys')
 
 const T = new Twit(twitConfigKeys)
 
+// Search tweets
+//
+// const paramsObj = { q: 'brexit', count: 5}
+// const gotData = (err, data, response) => {
+//   const tweets = data.statuses
+//   tweets.forEach(tweet => console.log(tweet.text))
+// }
+
+// T.get('search/tweets', paramsObj, gotData)
+
+// Write tweets
+//
+const tweetIt = tweetStatus => {
+  const randomNumber = Math.floor(Math.random()*100)
+  const tweet = {
+    status: 'This is a random number: ' + randomNumber + '  #twitterBot #NodeJS'
+  }
+  const tweeted = (err, data, response) => err ? console.log('Something went wrong:', err.message) : console.log('It worked!!')
+  
+  T.post('statuses/update', tweet, tweeted)
+}
+
+setInterval(tweetIt, 1000*60)
+
 console.log('TWITTER BOT STARTED!!')
