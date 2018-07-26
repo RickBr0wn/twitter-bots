d38fc55b5c711a618038e85165c0b18ea8be676c
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-26 13:33:34 +0100

    Updated to followBot (untested)

--- shiffmans-twitter-bot/bot.js
+++ shiffmans-twitter-bot/bot.js
@@ -3,7 +3,7 @@ const twitConfigKeys = require('./config/keys')
 
 const T = new Twit(twitConfigKeys)
 
-// Search tweets
+// Search tweets :: GET()
 //
 // const paramsObj = { q: 'brexit', count: 5}
 // const gotData = (err, data, response) => {
@@ -13,18 +13,40 @@ const T = new Twit(twitConfigKeys)
 
 // T.get('search/tweets', paramsObj, gotData)
 
-// Write tweets
+// Write tweets :: POST()
 //
-const tweetIt = tweetStatus => {
-  const randomNumber = Math.floor(Math.random()*100)
+// const tweetIt = tweetStatus => {
+//   const randomNumber = Math.floor(Math.random()*100)
+//   const tweet = {
+//     status: 'This is a random number: ' + randomNumber + '  #twitterBot #NodeJS'
+//   }
+//   const tweeted = (err, data, response) => err ? console.log('Something went wrong:', err.message) : console.log('Tweet sent: ' + tweet.status) 
+//   T.post('statuses/update', tweet, tweeted)
+// }
+
+// Loop Tweets
+//
+// setInterval(tweetIt, 1000*60)
+
+// Event tweets :: STREAM()
+// Tweets when followed
+//
+const tweetIt = (tweetStatus, screenName) => {
   const tweet = {
-    status: 'This is a random number: ' + randomNumber + '  #twitterBot #NodeJS'
+    status: tweetStatus
   }
-  const tweeted = (err, data, response) => err ? console.log('Something went wrong:', err.message) : console.log('It worked!!')
-  
+  const tweeted = (err, data, response) => err ? console.log('Something went wrong:', err.message) : console.log('Tweet sent to: ' + screenName) 
   T.post('statuses/update', tweet, tweeted)
 }
 
-setInterval(tweetIt, 1000*60)
+const followed = eventMsg => {
+  const name = eventMsg.source.name
+  const screenName = eventMsg.source.screen_name
+
+  tweetIt('@' + screenName + ' Thanks ' + name + ' for following me!!', screenName)
+}
+
+let stream = T.stream('user')
+stream.on('follow', followed)
 
 console.log('TWITTER BOT STARTED!!')
